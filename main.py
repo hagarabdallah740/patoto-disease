0ffd47b176ae653185aadaadb951b3ecf33097cc
hagarabdallah740 <hagarabdallah740@gmail.com>
2023-10-22 17:41:13 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import keras
 from tensorflow import keras
 models = keras.models
@@ -22,40 +23,40 @@ class_names = dataset.class_names
 # print(len((dataset)))
 
 plt.figure(figsize=(10,10))
-# plt.figure(figsize=(10,10))
-# for image_batch, label_batch in dataset.take(1):
-#     for i in range(12):
-#         ax = plt.subplot(3,4,i+1)
-#         plt.imshow(image_batch[i].numpy().astype("uint8"))
-#         plt.axis("off")
-#         plt.title(class_names[label_batch[i]])
-#     plt.show()
-# train_size=0.8
-# print(len(dataset)*train_size)
-# train_ds=dataset.take(54)
-# print(len(train_ds))         # output---->54
-# train_ds = dataset.skip(54)
+plt.figure(figsize=(10,10))
+for image_batch, label_batch in dataset.take(1):
+    for i in range(12):
+        ax = plt.subplot(3,4,i+1)
+        plt.imshow(image_batch[i].numpy().astype("uint8"))
+        plt.axis("off")
+        plt.title(class_names[label_batch[i]])
+    # plt.show()
+train_size=0.8
+print(len(dataset)*train_size)
+train_ds=dataset.take(54)
+print(len(train_ds))         # output---->54
+train_ds = dataset.skip(54)
 # print(len(train_ds))     output------>14   (68-54)
 
 
-# def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
-#     assert (train_split + test_split + val_split) == 1
-#
-#     ds_size = len(ds)
-#
-#     if shuffle:
-#         ds = ds.shuffle(shuffle_size, seed=12)
-#
-#     train_size = int(train_split * ds_size)
-#     val_size = int(val_split * ds_size)
-#
-#     train_ds = ds.take(train_size)
-#     val_ds = ds.skip(train_size).take(val_size)
-#     test_ds = ds.skip(train_size).skip(val_size)
-#
-#     return train_ds, val_ds, test_ds
-# train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
-#
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+
+    ds_size = len(ds)
+
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+
+    train_ds = ds.take(train_size)
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+
+    return train_ds, val_ds, test_ds
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+
 # -----------------------------------------------------------------------------------
 
 # Creating a Layer for Resizing and Normalization
@@ -95,4 +96,55 @@ model = models.Sequential([
 model.build(input_shape=input_shape)
 print(model.summary())
 
+model.compile(
+    optimizer='adam',
+    # loss function is commonly used in classification problems
+    # The from_logits=False argument indicates that the predicted probabilities are not in the form of raw logits,
+    # but rather they are already converted to probabilities using a softmax activation function.
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+history = model.fit(
+    train_ds,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    verbose=1,
+    validation_data=val_ds
 
+)
+print(history.history['accuracy'])             # Training accuracy
+print(history.history['val_accuracy'])         # Validation accuracy
+
+scores = model.evaluate(test_ds)
+print(scores)
+print(history.params)
+# ---------------------------------------------
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(range(EPOCHS), acc, label='Training Accuracy')
+plt.plot(range(EPOCHS), val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(EPOCHS), loss, label='Training Loss')
+plt.plot(range(EPOCHS), val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+for images_batch, labels_batch in test_ds.take(1):
+    first_image = images_batch[0].numpy().astype('uint8')
+    first_label = labels_batch[0].numpy()
+
+    print("first image to predict")
+    plt.imshow(first_image)
+    print("actual label:", class_names[first_label])
+
+    batch_prediction = model.predict(images_batch)
+    print("predicted label:", class_names[np.argmax(batch_prediction[0])])
